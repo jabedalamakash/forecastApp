2766f70557c507328aefc8f3e31a9839a39e324d
jabedalamakash <jabedalamakash00@gmail.com>
2023-08-18 06:49:55 +0600

    finally done

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -7,21 +7,15 @@ import '../globals.css';
 const Screen = () => {
 
               const [query,setQuery] =useState('dhaka');
-              const[apiData,setApiData] = useState(null);
-
-              const key="9fda3e28bdbf04488a129f72b7e5852d"
-
-          
-              
-              
+              const[apiData,setApiData] = useState();
 
 
               const handleData =  async() => {
-              const res=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${key}&units=metric`);
+              const res=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=9fda3e28bdbf04488a129f72b7e5852d&units=metric`);
               const data= await res.json();
               setApiData(data);}
 
-            // console.log(apiData);
+            //  console.log(apiData);
 
 
             const handleChange = (e) => {
@@ -32,30 +26,30 @@ const Screen = () => {
 
             const handleSubmit = (e) => {
               e.preventDefault();
+              setQuery("")
               handleData();
             };
 
 
               useEffect(()=>{
                   handleData();
+                  },[]);
 
-              },[]);
 
-
-           
+             
                 
     return (
              
-<div className=" w-full mx-auto  overflow-hidden ">
+      <div className=" w-full mx-auto  overflow-hidden ">
 
-      <div>
-        <Image 
-        src="/bg-img.jpg"
-        width={1280}
-        height={500}
-        className="fixed object-cover object-center w-full h-full "
-        alt="background image"
-        />
+            <div>
+              <Image 
+              src="/bg-img.jpg"
+              width={1440}
+              height={500}
+              className="fixed object-cover object-center w-screen h-full "
+              alt="background image"
+              />
 
 
       </div>
@@ -70,7 +64,8 @@ const Screen = () => {
                 className=" bg-white/10 border border-t-0 border-l-0 border-opcity-30 text-2xl shadow-5xl text-white mx-auto  px-6 py-4 mb-5 sm:mb-0  w-full sm:w-[100%]   rounded-lg sm:mr-5 text-left "
                 value={query} 
                 placeholder='Enter city name ' required
-                onChange={handleChange} />
+                onChange={handleChange}
+               />
 
 
                 <button 
@@ -86,37 +81,38 @@ const Screen = () => {
 
         
 
-      
-      
+     <div>
+           {!apiData? <h1>Loading...</h1>:
+                  
+                    
+                    <div className="w-full px-10 sm:px-0 mx-auto ">
 
-     { !apiData ? (
-      <h1> No Data Found</h1>
-     ):(
-
-            
-             <div className="w-full px-10 sm:px-0 mx-auto ">
-
-            <div className=' relative mt-20 w-full   text-center m-20 sm:w-1/2 h-full p-20 mx-auto bg-white/10 shadow-5xl border border-r-0 border-b-0 border-opcity-30  rounded-3xl'>
-              
-             <Image
-              src={`http://openweathermap.org/img/wn/${apiData.weather[0].icon}@2x.png`}
-              width={100}
-              height={100}
-              className='  block px-10 sm:px-0 mx-auto' alt="icon" /> 
-            
-                
-              <h1 className=" text-4xl text-white text-center pb-3 font-semibold  block">{apiData.name}, {apiData.sys.country}</h1> 
-                <h3><span className=" block text-white pb-3 font-semibold text-3xl text-center ">{apiData.main.temp} °C</span></h3>
-                <h1 className= " block text-3xl text-center font-semibold  text-white ">{apiData.weather[0].main}</h1> 
-
-                </div> 
-
-                </div>
-        )} 
+                    <div className=' relative mt-20 w-full   text-center m-20 sm:w-1/2 h-full p-20 mx-auto bg-white/10 shadow-5xl border border-r-0 border-b-0 border-opcity-30  rounded-3xl'>
+                   
+                     {apiData.name?  <Image
+                      src={`http://openweathermap.org/img/wn/${apiData.weather[0].icon}@2x.png`}
+                      width={100}
+                      height={100}
+                      className='  block px-10 sm:px-0 mx-auto' alt="icon" /> : null}
  
-</div>  
+                      {apiData.name? <h1 className=" text-4xl text-white text-center pb-3 font-semibold  block">{apiData.name}, {apiData.sys.country}</h1>:null}
+                      {apiData.main? <h3><span className=" block text-white pb-3 font-semibold text-3xl text-center ">{apiData.main.temp} °C</span></h3>  :null}
+                       {apiData.weather?<h1 className= " block text-3xl text-center font-semibold  text-white ">{apiData.weather[0].main}</h1>:null} 
+                       {apiData.message?<h1 className=" text-4xl text-red-700 font-bold text-center">{apiData.message}</h1>:null}
+
+                        </div> 
+
+                        </div>}
+      
+
+      </div>  
+  
+     </div>  
             
     );
 };
 
 export default Screen;
+
+
+
--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+
+// require(".env").config();
+
 const nextConfig = {
     reactStrictMode: true,
     images : {
