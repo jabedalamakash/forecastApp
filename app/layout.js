8678267a271faf153eea600c677b4403a56c199a
jabedalamakash <jabedalamakash00@gmail.com>
2023-08-19 07:34:34 +0600

    indexing

--- app/layout.js
+++ app/layout.js
@@ -6,16 +6,25 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
+
+// const google-site-verificationl="zUHOr9PriOvBWZMPsfz-uxclsnqbVyeIx12M9idR9SM"
+
 export const metadata = {
-  title: 'Sky Weather',
-  description: 'Get Your Current Weather Update',
-}
+ title: 'Sky Weather',
+ description: 'Get Your Current Weather Update',
+ google: "zUHOr9PriOvBWZMPsfz-uxclsnqbVyeIx12M9idR9SM",
+ 
+ }
+
+
+
+ 
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-      <meta name="google-site-verification" content="zUHOr9PriOvBWZMPsfz-uxclsnqbVyeIx12M9idR9SM" />
+      <meta name="google" content="zUHOr9PriOvBWZMPsfz-uxclsnqbVyeIx12M9idR9SM" />
       </Head>
       <body className={inter.className}>{children}
       
