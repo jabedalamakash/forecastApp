5e622ebfce014e0a969723edad3cecdb4c3e232e
jabedalamakash <jabedalamakash00@gmail.com>
2023-08-21 11:59:50 +0600

    sitemap added

---
+++ app/sitemap.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const sitemap = () => {
+    return [
+        {
+            url: 'https://skyweather.vercel.app',
+            lastModified: new Date(),
+          },
+    ];
+};
+
+export default sitemap;
